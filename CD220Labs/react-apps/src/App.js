9173017c6193cbd596e57a0436e9f670f8a6dd25
Linda Angulo Lopez <angulo.linda@gmail.com>
2022-02-12 22:40:43 +0100

    changed to table

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -8,14 +8,19 @@ class App extends React.Component {
     let url = "https://reststop.randomhouse.com/resources/authorevents/?start=0&max=3&expandLevel=1&isbn=0&authorid=0"
     const req = axios.get(url);
     console.log(req);
+
     req.then(resp => {
          let listOfEvents = resp.data.authorEvent;
         let listOfEventsAsArray = Object.entries(listOfEvents);
         let eventDetails = listOfEventsAsArray.map((eventDetial)=>{
           let eventListCollection = Object.entries(eventDetial[1])
-          return <div>{eventListCollection[4][1]} on {eventListCollection[5][1]} at {eventListCollection[7][1]} in {eventListCollection[10][1]} - {eventListCollection[11][1]}</div>
+          return <tr><td style={{color: "red",border: "1px solid black"}}>{eventListCollection[4][1]} </td>
+          <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[5][1]} </td>
+          <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[7][1]}</td>
+          <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[10][1]}</td>
+          <td style={{color: "red",border: "1px solid black"}}>{eventListCollection[11][1]}</td></tr>
         })
-        this.setState({eventList:eventDetails})
+        this.setState({eventList:<table style={{border: "1px solid black"}}><tbody>{eventDetails}</tbody></table>})
       })
     .catch(err => {
         console.log(err.toString())
@@ -27,17 +32,15 @@ class App extends React.Component {
     let li_ctr = 0;
     return (
       <div style={colorStyle}>
-        React Component
-            <ul>
-                {this.state.eventList.map(function(eventDetails){
-                    return <li key={li_ctr++}>{eventDetails}</li>;
-                  })}
-            </ul>      
+        Events List
+        <br/>
+            {
+            this.state.eventList
+            }
         </div>
     );
   }
 }
 
 export default App;
-
 
