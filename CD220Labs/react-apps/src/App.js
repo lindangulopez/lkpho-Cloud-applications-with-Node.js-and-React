273520f69823bc01e0efa31546bd11ff575221f6
Linda Angulo Lopez <angulo.linda@gmail.com>
2022-02-12 22:16:57 +0100

    change state on click

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -5,20 +5,26 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
+  state = { counter:0 }
+
+  incrementCounter = ()=> {
+    this.setState({counter:this.state.counter+1});
+  }
 
   render() {
-    const colorStyle = {
-    color:this.props.color,
-    fontSize:this.props.size+"px"
-    }
+    const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
     return (
-      <div>
-        <div style={colorStyle}>
-        Hello World!
-        </div>
+      <div style={colorStyle}>
+        React Component
+        <br /><br />
+        <button onClick={this.incrementCounter}>Click Me!</button>
+        <br /><br />
+        {this.state.counter}
       </div>
     );
   }
 }
 
 export default App;
+
+
