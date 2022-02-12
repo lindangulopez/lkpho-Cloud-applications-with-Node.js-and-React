7a15fbf35739faa83a7a25e4b9477f0565a61f11
Linda Angulo Lopez <angulo.linda@gmail.com>
2022-02-12 22:13:39 +0100

    change the function to a class

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -1,16 +1,24 @@
+import React from 'react';
 
-function App(props) {
-  const colorStyle = {
-    color:props.color,
-    fontSize:props.size+"px"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div>
-      <div style={colorStyle}>
-      Hello World!
+
+  render() {
+    const colorStyle = {
+    color:this.props.color,
+    fontSize:this.props.size+"px"
+    }
+    return (
+      <div>
+        <div style={colorStyle}>
+        Hello World!
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
